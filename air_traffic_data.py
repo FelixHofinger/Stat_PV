8c70ce420172fceb68bc99592d3c8046d4f88705
hofife <felix.hofinger@tugraz.at>
2022-01-26 17:31:30 +0100

    Added Flugverkehr

---
+++ air_traffic_data.py
@@ -0,0 +1,200 @@
+################################################################################
+# File: binnenflug_matrix.py
+#
+# Date: 14.1.2022
+# Author: Florian Lammer <florian.lammer@tugraz.at>
+#
+# Description:
+#   Plottet eine Heatmap für alle Binnenflüge in Österreich zwischen den 6 Flughäfen.
+#   Zahlen werden vom ankommenden und abgehenden Flughafen berichtet. Nach Statistik
+#   Austria ist hier der Mittelwert zu bilden, da es zu Abweichungen kommen kann.
+#
+#   Plots können mittels der Parameter YEAR und Q für jedes Quartal jedes Jahres erstellt
+#   werden. Siehe die Code-Sektion "plot data".
+################################################################################
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Air_Traffic_Data:
+    def __init__(self, year1=None, year2=None, agg_level=None):
+        self._year1 = year1
+        self._year2 = year2
+        self._agg_level = agg_level
+        self._comp_year = (self._year1, self._year2)
+        self._month = {1: ('01', 'Jänner', 'Q1'),
+                       2: ('02', 'Februar', 'Q1'),
+                       3: ('03', 'März', 'Q1'),
+                       4: ('04', 'April', 'Q2'),
+                       5: ('05', 'Mai', 'Q2'),
+                       6: ('06', 'Juni', 'Q2'),
+                       7: ('07', 'Juli', 'Q3'),
+                       8: ('08', 'August', 'Q3'),
+                       9: ('09', 'September', 'Q3'),
+                       10: ('10', 'Oktober', 'Q4'),
+                       11: ('11', 'November', 'Q4'),
+                       12: ('12', 'Dezember', 'Q4')}
+        self._cross_section = {}
+
+    # read and preprocess data
+    def read_air_tr_data(self, airport):
+        path = f"./air_traffic_data/"
+        flug_ab = pd.read_excel(f'{path}Binnenflug_aut_abgehend.xlsx', engine='openpyxl', sheet_name='aufbereitet').replace('-',0)
+        flug_an = pd.read_excel(f'{path}Binnenflug_aut_ankommend.xlsx', engine='openpyxl', sheet_name='aufbereitet').replace('-',0)
+
+
+
+        # create a list of all existing airports and years
+        flughafen_list = list(flug_ab['Berichtshafen'].unique())
+        year_list = list(flug_ab['Jahr'].unique())
+
+        # create dictionarys to store values from nested loops
+        # some nested dictionaries are created in the nested loops
+        # data structure: dict_type --> dict_year --> dict_quarter --> matrix with data
+        dict_type = dict()
+
+        # maximum values in all data to set plot color bar always to same boundaries
+        list_v_max_plot = list()
+
+        for TYPE in ['Passagiere nach Streckenzielen', 'Flüge']:
+            # initialise values
+            dict_year = dict()
+            pass_max = 0
+            for YEAR in year_list:
+                # initialise values
+                print(YEAR)
+                dict_quarter = dict()
+                for Q in range(1, 5):
+                    print(Q)
+                    # create a emtpy matrix for this year-quarter combination
+                    matrix = pd.DataFrame(0, columns=flughafen_list, index=flughafen_list)
+
+                    # loop thorugh all airports
+                    for i in range(0, len(flughafen_list)):
+                        for j in range(0, len(flughafen_list)):
+                            berichtshafen = flughafen_list[i] #[1] # Graz
+                            starthafen = flughafen_list[j] #[0] # Wien
+
+                            # ankommend Berichtshafen
+                            val_1 = flug_an[(flug_an['Jahr'] == YEAR ) & (flug_an['Quartal'] == Q) & (flug_an['Berichtshafen'] == berichtshafen) & (flug_an['Starthafen'] == starthafen)][TYPE].item()
+                            # abgehend Starthafen
+                            val_2 = flug_ab[(flug_ab['Jahr'] == YEAR ) & (flug_ab['Quartal'] == Q) & (flug_ab['Berichtshafen'] == starthafen) & (flug_ab['Landehafen'] == berichtshafen)][TYPE].item()
+                            # create mean value between data reported from both airports
+                            if airport == 'Alle':
+                                val = int(np.round((val_1+val_2)/2))
+                            elif berichtshafen == airport or starthafen == airport:
+                                val = int(np.round((val_1+val_2)/2))
+                            else:
+                                val = 0
+                            # fill matrix at the current cell
+                            matrix.loc[starthafen,berichtshafen] = val
+                            # if a new maximum value is found, update max value for plot
+                            if val > pass_max:
+                                pass_max = val
+                            else:
+                                pass
+
+                    # print(matrix)
+                    # write matrix to dictonary data storing structure
+                    dict_quarter[Q] = matrix.copy()
+                    del matrix
+                dict_year[YEAR] = dict_quarter
+            dict_type[TYPE] = dict_year
+            # append list with max value for each type (passengers or flights
+            list_v_max_plot.append(pass_max)
+
+        return dict_type, dict_year
+
+    def barplot_autolabel(self, ax, bar, text, height, bottom_height):
+        """Attach a text label above each bar in *rects*, displaying its height."""
+
+        for bars in bar:
+            ax.annotate('{}'.format(text),
+                        xy=(bars.get_x() + bars.get_width(), height + bottom_height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom', rotation=0, size=15)
+
+    def plot_air_tr_data(self, data, year1, year2, indicator, airport):
+        dict_indicator = {'Anzahl Passagiere': 'Passagiere nach Streckenzielen',
+                          'Anzahl Flüge': 'Flüge',
+                          'Flugkilometer': 'Flüge',
+                          'Personenkilometer':'Passagiere nach Streckenzielen'}
+
+        path = f"./air_traffic_data/"
+        dist_mat = pd.read_excel(f'{path}Distanzmatrix_Binnenflugverkehr.xlsx', engine='openpyxl').iloc[:, 1:].set_index('Flughafen')
+
+        plot_data_li = []
+        for YEAR in [year1, year2]:
+            df_leistung = pd.DataFrame({'flug/pers/km': [0, 0, 0, 0]}, index=['Q1', 'Q2', 'Q3', 'Q4'])
+            for Q_i in range(1, 5):
+                plot_data = data[dict_indicator[indicator]][YEAR][Q_i]
+
+                if indicator in ['Flugkilometer', 'Personenkilometer']:
+                    data_value = (dist_mat * plot_data).sum().sum().round()
+                else:
+                    data_value = (1 * plot_data).sum().sum().round()
+                df_leistung.loc[f'Q{Q_i}', 'flug/pers/km'] = data_value
+
+            df_leistung.reset_index(inplace=True, drop=True)
+            plot_data_li.append(df_leistung)
+
+        y_max = max(plot_data_li[0]['flug/pers/km'].max(), plot_data_li[1]['flug/pers/km'].max())
+
+        abs_diff =  []
+        total = []
+        plt.style.use('seaborn-darkgrid')
+        fig, ax = plt.subplots()
+        width = 0.28
+
+        for no in range(len(plot_data_li[0])):
+            data_y1 = plot_data_li[0].iloc[no]['flug/pers/km']
+            data_y2 = plot_data_li[1].iloc[no]['flug/pers/km']
+            bar_1 = ax.bar(no + 1 - width / 2, data_y1, width, color='darkred',
+                           edgecolor='black', linewidth=1.5)
+            bar_2 = ax.bar(no + 1 + width / 2, data_y2, width, color='darkgreen',
+                           edgecolor='black', linewidth=1.5)
+
+            plot_height = max(data_y1, data_y2)
+            rel_month_diff_car = round(((int(data_y2) - int(data_y1)) / int(data_y1)) * 100, 2)
+            abs_diff.append((int(data_y2) - int(data_y1)))
+            total.append(int(data_y1))
+            self.barplot_autolabel(ax, bar_1, f'{rel_month_diff_car} %', plot_height, plot_height/20)
+
+        # 4 Quartale
+        ax.set_xticks(np.arange(1, 5))
+        ax.set_xticklabels(np.arange(1, 5), size=13)
+
+        ax.tick_params(axis='y', labelsize=13)
+        ax.set_ylabel(f'{indicator}', size=15)
+        ax.set_xlabel('Quartal', size=15)
+        ax.set_ylim(0, y_max * 1.25)
+        ax.grid(True)
+        ax.set_title(f'Übersicht Verkehrsaufkommen', size=25, weight='bold', position=(0.5, 1.065))
+        ax.legend((bar_1, bar_2),
+                (f'{indicator} - {year1}', f'{indicator} - {year2}'), fontsize=15)
+        rel = round(sum(abs_diff) / sum(total) * 100, 2)
+        ax.text(0.025, 0.95, f'Veränderung {indicator}: {rel} %', horizontalalignment='left',
+                verticalalignment='top', transform=ax.transAxes, fontsize=15, weight='bold')
+        fig.set_size_inches(18, 8)
+        return fig
+
+
+            #bar_car_y1 = ax.bar(no1 + 1 - width / 2, int(car_data_y1), width, color='darkred',
+                    #                     edgecolor='black', linewidth=1.5)
+                    # bar_hgv_y1 = ax.bar(no1 + 1 - width / 2, int(hgv_data_y1), width, color='lightblue',
+                    #                     edgecolor='black', linewidth=1.5, bottom=int(car_data_y1))
+                    # bar_car_y2 = ax.bar(no1 + 1 + width / 2, int(car_data_y2), width, color='darkgreen',
+                    #                     edgecolor='black', linewidth=1.5)
+                    # bar_hgv_y2 = ax.bar(no1 + 1 + width / 2, int(hgv_data_y2), width, color='darkorange',
+                    #                     edgecolor='black', linewidth=1.5, bottom=int(car_data_y2))
+                    #
+
+
+        return plot_data_li
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-from gui import Gui
-
-import os
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 
 from cross_section_data import Cross_Section_Data
+from air_traffic_data import Air_Traffic_Data
 
-#from symul8_sumo_simulator import Symul8SumoSimulator
-#from symul8_vissim_simulator import Symul8VissimSimulator
-
-__author__ = ("Felix Hofinger", "Florian Lammer")
+__author__ = "Felix Hofinger"
 __copyright__ = ""
-__credits__ = ("Felix Hofinger", "Florian Lammer")
+__credits__ = "Felix Hofinger"
 __license__ = ""
 __version__ = "0.0.1"
-__maintainer__ = ("Felix Hofinger", "Florian Lammer")
-__email__ = ("felix.hofinger@tugraz.at", "florian.lammer@tugraz.at")
+__maintainer__ = "Felix Hofinger"
+__email__ = "felix.hofinger@tugraz.at"
 __status__ = "Development"
-
 
 class StatistikPV_Gui():
     def __init__(self, *, appbrandimage=None, appicon=None, apptitle="", appversion="", appcopyrightlogos=[]):
@@ -30,25 +23,22 @@ class StatistikPV_Gui():
         self._appcopyrightlogos = appcopyrightlogos
 
     def create(self, data_cat):
-
         # default layout
         st.set_page_config(page_title=self._apptitle, page_icon=self._appicon, layout='wide', initial_sidebar_state='auto')
         if self._appbrandimage:
             for logo in self._appbrandimage:
                 st.sidebar.image(logo)
-
         elif self._apptitle:
             st.sidebar.title(self._apptitle)
-
         if self._appversion:
             st.sidebar.text(f"Version {self._appversion}")
-
         for logo in self._appcopyrightlogos:
             st.sidebar.image(logo)
-
         datacat = st.sidebar.selectbox('Kategorie', data_cat)
         st.title(self._apptitle)
-        if datacat == 'Mobilitätsdaten - Allgemein':
+
+        # Querschnittsdaten (Segmentbezogen)
+        if datacat == 'Querschnittsdaten (Segment)':
             st.header('Querschnittsdaten-Berechnung (Segmentbezogen)')
             st.session_state.year1 = st.selectbox("Vergleichsjahr - 1", np.arange(2010,2025))
             st.session_state.year2 = st.selectbox("Vergleichsjahr - 2", np.arange(2010,2025))
@@ -65,13 +55,10 @@ class StatistikPV_Gui():
                 except:
                     st.subheader(f'Keine Daten für das Jahr {st.session_state.year1} bzw. {st.session_state.year2} vorhanden!')
             if st.session_state.cross_section_selected:
-                st.write(cross_data)
-                data_to_plot, driving_directions_li = cross_section_data.select_drivingdirection(cross_data,
-                                                                                           st.session_state.cross_section_selected)
+                data_to_plot, driving_directions_li = cross_section_data.select_drivingdirection(cross_data, st.session_state.cross_section_selected)
                 st.session_state.driving_direction_select = st.selectbox("Fahrtrichtung wählen", driving_directions_li)
-                st.session_state.agg_level = st.selectbox("Auswertungsintervall",['Monatlich','Quartalsweise'])
-                st.session_state.weekday = st.selectbox("Wochentag", ['Montag-Freitag', 'Samstag','Sonn- und Feiertag'])
-
+                st.session_state.agg_level = st.selectbox("Auswertungsintervall", ['Monatlich','Quartalsweise'])
+                st.session_state.weekday = st.selectbox("DTV-Wert", ['Montag-Freitag', 'Samstag','Sonn- und Feiertag'])
                 plot_data = st.button("Vergleich Querschnittsdaten anzeigen")
 
                 if plot_data:
@@ -81,8 +68,7 @@ class StatistikPV_Gui():
                                                                 st.session_state.weekday)
                     st.pyplot(fig)
 
-        #elif datacat == 'Mobilitätsdaten - Aktive Mobilität':
-            # Section Querschnittsdaten - Berechnung (Raumbezogen)
+        elif datacat == 'Querschnittsdaten (Raum)':
             st.header('Querschnittsdaten - Berechnung (Raumbezogen)')
             st.session_state.y1_raum = st.selectbox("Vergleichsjahr - 1 ", np.arange(2010, 2025))
             st.session_state.y2_raum = st.selectbox("Vergleichsjahr - 2 ", np.arange(2010, 2025))
@@ -92,7 +78,7 @@ class StatistikPV_Gui():
                                                                       'Steiermark' , 'Oberösterreich', 'Salzburg',
                                                                       'Kärnten', 'Tirol', 'Vorarlberg'])
             st.session_state.agg_level_raum = st.selectbox("Auswertungsintervall", ['Monatlich', 'Quartalsweise'])
-            st.session_state.weekday_raum = st.selectbox("Wochentag", ['Montag-Freitag', 'Samstag', 'Sonn- und Feiertag'])
+            st.session_state.weekday_raum = st.selectbox("DTV-Wert", ['Montag-Freitag', 'Samstag', 'Sonn- und Feiertag'])
             plot_data_raum = st.button("Vergleich Querschnittsdaten anzeigen")
 
             cross_section_data_raum = Cross_Section_Data(year1=st.session_state.y1_raum, year2=st.session_state.y2_raum)
@@ -104,32 +90,56 @@ class StatistikPV_Gui():
 
                 st.pyplot(fig)
 
+        elif datacat == 'Flugverkehr':
+            st.header('Auswertung Flugverkehr')
+            st.session_state.year1 = st.selectbox("Vergleichsjahr - 1", np.arange(2010, 2025))
+            st.session_state.year2 = st.selectbox("Vergleichsjahr - 2", np.arange(2010, 2025))
+            st.session_state.airport = st.selectbox("Flughafen", ['Alle', 'Wien', 'Graz', 'Linz', 'Salzburg', 'Innsbruck', 'Klagenfurt'])
+            st.session_state.indicator = st.selectbox("Indikator", ['Anzahl Passagiere', 'Anzahl Flüge', 'Flugkilometer', 'Personenkilometer'])
+            st.session_state.agg_level_raum = st.selectbox("Auswertungsintervall", ['Quartalsweise', 'Monatlich (nicht untersützt)'])
 
+            plot_data = st.button("Flugverkehrsdaten auswerten")
+            if plot_data:
+                with st.spinner("Auswertung Flugverkehrsdaten läuft"):
+                    air_traffic = Air_Traffic_Data()
+                    data_dict_type, data_dict_year = air_traffic.read_air_tr_data(st.session_state.airport)
+                    print(st.session_state.year1, type(st.session_state.year1), data_dict_year.keys())
+
+                    if st.session_state.year1 not in data_dict_year.keys() or st.session_state.year2 not in data_dict_year.keys():
+                        st.warning('Jahr ist nicht in Datensatz enthalten - Können im Statistik Austria Statcube heruntergeladen werden')
+                    else:
+                        fig = air_traffic.plot_air_tr_data(data_dict_type,
+                                                           st.session_state.year1,
+                                                           st.session_state.year2,
+                                                           st.session_state.indicator,
+                                                           st.session_state.airport)
+
+                        st.pyplot(fig)
+
+
+
+
+
+
+
+        elif datacat == 'aktive Mobilität':
             st.header('Radverkehr')
-            with st.expander('Radverkehr'):
 
-                st.header('2019')
-                graz_2019 = st.number_input('# Fahrradfahrer Graz 2019')
-                wien_2019 = st.number_input('# Fahrradfahrer Wien 2019')
+            st.header('2019')
+            graz_2019 = st.number_input('# Fahrradfahrer Graz 2019')
+            wien_2019 = st.number_input('# Fahrradfahrer Wien 2019')
 
-                st.header('2020')
-                graz_2020 = st.number_input('# Fahrradfahrer Graz 2020')
-                wien_2020 = st.number_input('# Fahrradfahrer Wien 2020')
+            st.header('2020')
+            graz_2020 = st.number_input('# Fahrradfahrer Graz 2020')
+            wien_2020 = st.number_input('# Fahrradfahrer Wien 2020')
+            st.header('2021')
 
-                st.header('2021')
-                graz_2021 = st.number_input('# Fahrradfahrer Graz 2021')
-                wien_2021 = st.number_input('# Fahrradfahrer Wien 2021')
+            graz_2021 = st.number_input('# Fahrradfahrer Graz 2021')
+            wien_2021 = st.number_input('# Fahrradfahrer Wien 2021')
 
-                chart_data_graz = pd.DataFrame([[graz_2019, wien_2019], [graz_2020, wien_2020], [graz_2021, wien_2021]], index=['2019', '2020', '2021'], columns=['Graz','Wien'])
-                #print(chart_data_graz)
-
-                st.line_chart(chart_data_graz)
-
-
-
-
-
-
+            chart_data_graz = pd.DataFrame([[graz_2019, wien_2019], [graz_2020, wien_2020], [graz_2021, wien_2021]], index=['2019', '2020', '2021'], columns=['Graz','Wien'])
+            #print(chart_data_graz)
+            st.line_chart(chart_data_graz)
 
 def main():
     # Project consortium logos
@@ -137,21 +147,17 @@ def main():
         r"./logos/bmk.png",  # TUGraz
         r"./logos/oebb.png",
         r"./logos/asfinag.png"# BMK
-
     ]
 
     an_logo = [
         r"./logos/TU_Graz.png",  # TUGraz
         #r"./logos/isv.png",
     ]
-
-    data_cat = ['Mobilitätsdaten - Allgemein',
-                'Mobilitätsdaten - Aktive Mobilität',
-                'Mobilitätsdaten - ÖPNV',
-                'Mobilitätsdaten - Flugverkehr',
-                'Energieverbrauch']
-
-
+    data_cat = ['Querschnittsdaten (Segment)',
+                'Querschnittsdaten (Raum)',
+                'Flugverkehr',
+                'städtischer Verkehr',
+                'aktive Mobilität']
 
     gui = StatistikPV_Gui(
         apptitle="Prototypisches Dashboard BMK Monitoring Personenverkehr",
@@ -160,7 +166,6 @@ def main():
         appcopyrightlogos=an_logo,
         appversion=__version__)
     gui.create(data_cat)
-
 
 if __name__ == '__main__':
     main()
